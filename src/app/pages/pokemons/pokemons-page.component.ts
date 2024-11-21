9f536d72a8bb6668689a887569682ae431c96505
pablo <pnunfe@gmail.com>
2024-11-21 19:48:20 +0100

    skeleton page

--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { PokemonCardComponent } from '../../pokemons/components/pokemon-card/pokemon-card.component';
 import { PokemonListComponent } from '../../pokemons/components/pokemon-list/pokemon-list.component';
+import { PokemonListSkeletonComponent } from '../../pokemons/components/ui/pokemon-list-skeleton/pokemon-list-skeleton.component';
 
 @Component({
   selector: 'pokemons-page',
-  imports: [CommonModule, PokemonListComponent, PokemonCardComponent],
+  imports: [CommonModule, PokemonListComponent, PokemonListSkeletonComponent],
   templateUrl: './pokemons-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- src/app/pokemons/components/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemons/components/pokemon-list/pokemon-list.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { PokemonCardComponent } from '../pokemon-card/pokemon-card.component';
+import { PokemonListSkeletonComponent } from '../ui/pokemon-list-skeleton/pokemon-list-skeleton.component';
 
 @Component({
   selector: 'pokemon-list',
-  imports: [CommonModule, PokemonCardComponent],
+  imports: [CommonModule, PokemonCardComponent, PokemonListSkeletonComponent],
   templateUrl: './pokemon-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
