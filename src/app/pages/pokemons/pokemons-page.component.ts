570b92cac38e5ff126dfd47f9e498c40d6ba2122
pablo <pnunfe@gmail.com>
2024-11-22 11:20:01 +0100

    pagination and logo

--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -23,6 +23,7 @@ export class PokemonsComponent {
   public offset = signal(0);
   public pokemonList = signal<Pokemon[]>([]);
   public isLoading = signal(false);
+  public totalPages = 60;
 
   pokemonListResource = resource({
     request: this.offset,
@@ -54,5 +55,24 @@ export class PokemonsComponent {
     if (this.offset() > 0) {
       this.offset.update((current) => current - 21);
     }
+  }
+
+  getPages(): number[] {
+    const currentPage = this.offset() / 21 + 1;
+    const pages: number[] = [];
+
+    for (
+      let i = Math.max(1, currentPage - 2);
+      i <= Math.min(this.totalPages, currentPage + 2);
+      i++
+    ) {
+      pages.push(i);
+    }
+
+    return pages;
+  }
+
+  goToPage(page: number) {
+    this.offset.set((page - 1) * 21);
   }
 }
