b53653d0cb7718ce8f3ed721ace0d3b5da95e0bc
pablo <pnunfe@gmail.com>
2024-11-28 16:22:09 +0100

    new title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, NavbarComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'pokemon-ssr';
+  title = 'pokemon-ssr-Angular19';
 }
